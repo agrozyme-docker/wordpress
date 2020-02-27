3608a804a645de55c823eb706390d1e1f465fcef
agrozyme <agrozyme@gmail.com>
2020-02-27 17:29:35 +0800

    add commands

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Summary
 
 The WordPress rich content management system can utilize plugins, widgets, and themes.
+
+# Commands
+
+- [wp](https://developer.wordpress.org/cli/commands/)
--- rootfs/usr/local/bin/docker-build.lua
+++ rootfs/usr/local/bin/docker-build.lua
@@ -1,6 +1,16 @@
 #!/usr/bin/lua
 local core = require("docker-core")
 
+local function wordpress_setup(bin)
+  local www = "/var/www"
+  local project = "/tmp/wordpress"
+  local version = "5.3.2"
+  core.run("%s/composer create-project --no-install --no-interaction johnpbloch/wordpress:%s %s", bin, version, project)
+  core.run("%s/composer --working-dir=%s install", bin, project)
+  core.run("tar -czf %s/wordpress.tgz -C %s .", www, project)
+  core.run("rm -rf %s", project)
+end
+
 local function wp_cli_setup(bin)
   core.run("wget -q -O %s/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar", bin)
   core.run("chmod +x %s/wp", bin)
@@ -8,16 +18,10 @@ end
 
 local function main()
   local bin = "/usr/local/bin"
-  local www = "/var/www"
-  local project = "/tmp/wordpress"
-  local version = "5.3.2"
-  wp_cli_setup(bin)
   core.run("apk add --no-cache lua-rex-pcre")
-  core.run("%s/composer create-project --no-install --no-interaction johnpbloch/wordpress:%s %s", bin, version, project)
-  core.run("%s/composer --working-dir=%s install", bin, project)
-  core.run("tar -czf %s/wordpress.tgz -C %s .", www, project)
+  wp_cli_setup(bin)
+  wordpress_setup(bin)
   core.run("%s/composer clear-cache", bin)
-  core.run("rm -rf %s", project)
 end
 
 main()
